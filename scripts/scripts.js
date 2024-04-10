0f5b11bdcd10764f396ba6e8929e59168b021000
Jedidiah-Miller <jedidiah.a.miller@gmail.com>
2024-04-10 15:13:26 -0700

    Chrome_search_image: initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,54 @@
+const URL_STORAGE_KEY = "chrome_image_search_URL"
+
+
+const googleSearchURL = (url) => {
+  return `https://www.google.com/searchbyimage?image_url=${url}&client=app`;
+};
+
+const googleLensSearchByImage = function (url) {
+  return `https://lens.google.com/uploadbyurl?url=${encodeURIComponent(url)}`;
+};
+
+const yandexSearchURL = (url) => {
+  return `https://yandex.com/images/search?source=collections&url=${url}&rpt=imageview`;
+};
+
+const bingSearchURL = (url) => {
+  return `https://www.bing.com/images/search?view=detailv2&iss=sbi&form=SBIVSP&sbisrc=UrlPaste&q=imgurl:${url}`;
+};
+
+function handleChange(e) {
+  const {value} = e.target;
+  setValue(value);
+}
+
+function setValue(value = null) {
+
+  document.getElementById("search-google-btn").href = googleSearchURL(value);
+  document.getElementById("search-google_lens-btn").href = googleLensSearchByImage(value);
+  document.getElementById("search-yandex-btn").href = yandexSearchURL(value);
+  document.getElementById("search-bing-btn").href = bingSearchURL(value);
+
+  localStorage.setItem(URL_STORAGE_KEY, value);
+}
+
+
+function setInitialValue() {
+  const value = localStorage.getItem(URL_STORAGE_KEY);
+  document.getElementById("url-input").value = value;
+  setValue(value);
+}
+
+
+function addEventListeners() {
+  document.getElementById("url-input").addEventListener("input", handleChange);
+}
+
+
+function initialize() {
+  addEventListeners();
+  setInitialValue();
+}
+
+
+initialize();
